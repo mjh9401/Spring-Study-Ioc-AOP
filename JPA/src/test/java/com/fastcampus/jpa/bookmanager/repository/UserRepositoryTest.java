23c5e03b7945332baeca93bac38e77db00da9bac
mjh9401 <mjh9401@naver.com>
2022-08-21 14:52:30 +0900

    Repository 실습3

--- JPA/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ JPA/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -22,54 +22,12 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-//    @Transactional : jpa getOne 메소드 사용시 세션유지를 위해 사용
     void crud() { // create, read, update, delete
-//        //List<User> users = userRepository.findAllById(Lists.newArrayList(1L,2L,3L));
-//        User user1 = new User("jack","jack@naver.com");
-//        User user2 = new User("do","do@naver.com");
-//        //userRepository.saveAll(Lists.newArrayList(user1,user2));
-//        userRepository.save(user1);
-//
-//        List<User> users = userRepository.findAll();
-//        users.forEach(System.out::println);
-
-//        User user = userRepository.findById(1L).orElse(null);
-//        System.out.println(user);
-//        userRepository.saveAndFlush(new User("afaf","afaf@naver.com"));
-//        userRepository.findAll().forEach(System.out::println); // flush는 db에 데이터를 넣는 시점에 대한 메소드
-        
-        // 데이터 갯수 확인
-        long count = userRepository.count();
-        System.out.println(count);
-        
-        // 해당 결과 존재여부 확인
-        boolean exists = userRepository.existsById(1L);
-        System.out.println(exists);
-        
-        // 데이터 삭제 delete는 select를 한번 하고 난뒤에 삭제
-        userRepository.deleteById(1L);
-        userRepository.deleteAll();
-        // deleteInBatch는 select 없이 delete 실행
-        userRepository.deleteInBatch(userRepository.findAllById(Lists.newArrayList(1L,3L)));
-        userRepository.deleteAllInBatch();
-
-        // 페이징
-        Page<User> users =userRepository.findAll(PageRequest.of(1,3));
-        System.out.println("page: "+users);
-        System.out.println("totalElements: "+users.getTotalElements());
-        System.out.println("totalpages: "+users.getTotalPages());
-        System.out.println("numberOfElements: "+users.getNumberOfElements());
-        System.out.println("sort: "+users.getSort());
-        System.out.println("size: "+users.getSize());
-
-        // matcher로 검색색
-        User user = new User();
-        user.setEmail("slow");
-
-        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("email", contains());
-        Example<User> example = Example.of(user, matcher);
-
-        userRepository.findAll(example).forEach(System.out::println);
+        userRepository.save(new User("david","dvid@naver.com"));
+        // insert와 update의 차이는 pk가 존재여부에 따라 save 메소드가 insert or update가 된다.
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("asdf@gmail.com");
+        userRepository.save(user);
 
     }
 
