397c3f272c9f19f7f1d6d011187829da83344458
mjh9401 <mjh9401@naver.com>
2022-09-10 20:27:02 +0900

    객체지향 프로그램 실습 -학점 계산기-

---
+++ oop-practice/src/main/java/org/example/calculate/AdditionOperator.java
@@ -0,0 +1,13 @@
+package org.example.calculate;
+
+public class AdditionOperator implements NewArithmeticOperator{
+    @Override
+    public boolean support(String operator) {
+        return "+".equals(operator);
+    }
+
+    @Override
+    public int calculate(PositiveNumber operend1, PositiveNumber operand2) {
+        return operend1.toInt()+operand2.toInt();
+    }
+}
